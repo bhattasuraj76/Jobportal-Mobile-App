144f10acd8518ad301db97b76540530a048f2dba
unknown <sanjaykhatri906@gmail.com>
2020-05-31 20:26:13 +0545

    radio btn

--- screens/profileStacks/BasicInfo.js
+++ screens/profileStacks/BasicInfo.js
@@ -18,20 +18,26 @@ import * as yup from "yup";
 import Input from "../../shared/input";
 import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
 import Icon from "../../shared/icon";
+import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from "react-native-simple-radio-button";
 
 //register validation schema
 const registerSchema = yup.object({
   firstName: yup.string().required("First name is required"),
   lastName: yup.string().required("Last name is required"),
-  address: yup.string().required("Password is required"),
-  phone: yup.string().required("Password is required"),
-  gender: yup.string().required("Password is required"),
+  address: yup.string().required("Phone number is required"),
+  phone: yup.string().required("Address is required"),
+  gender: yup.string().required("Bio is required"),
 });
+
+
 
 function BasicInfo({ navigation }) {
   const { isThemeDark } = useContext(ThemeContext);
   const { authUser, setAuthStatus } = useContext(AuthContext);
   const [isEditing, setIsEditing] = useState(false);
+
+
+  
 
   const editBasicInfo = () => {
     console.log("fasd");
@@ -43,19 +49,34 @@ function BasicInfo({ navigation }) {
     console.log("fasd");
   };
 
-  const updateBasicInfo = (values, actions) => {
+  const updateBasicInfo = (values) => {
+    console.log(values);
     const { firstName, lastName, gender, address, phone, description } = values;
 
     const resp = 1; //response form server
     if (resp == 1) {
       //reset form
-      actions.resetForm();
+      // actions.resetForm();
       //alert user
       alert("Updated Basic Info Successfully");
     } else {
       alert("Update Failed");
     }
   };
+
+  const updateGender=(handleChange,value)=>{
+    handleChange(value);
+
+
+
+  }
+
+  const gender=[
+    {label:"male", value:"male"},
+    {label:"female",value:"feamle"},
+    {label:"Others",value:"non"},
+  
+  ]
 
   return (
     <ContainerFluid>
@@ -74,9 +95,9 @@ function BasicInfo({ navigation }) {
               initialValues={{
                 firstName: "dafa",
                 lastName: "fasf",
-                phone: "fsaddfasd",
+                phone: "12345",
                 address: "fasdfa",
-                gender: "fdsafa",
+                gender: "male",
                 description: "fsdafas",
               }}
               validationSchema={registerSchema}
@@ -102,33 +123,39 @@ function BasicInfo({ navigation }) {
                       justifyContent: "space-evenly",
                     }}
                   >
-                    <Text>Personal Details</Text>
+                    <Text style={{
+                      borderBottomColor:'red',
+                      borderRadius:1,
+                      borderBottomWidth:2,
+                      fontWeight:'bold',
+                      borderBottomColor:'black',
+                      fontSize:20,
+                    }}>Personal Details</Text>
 
                     {!isEditing ? (
-                      <TouchableOpacity onPress={() => setIsEditing(true)}>
-                        <Icon name="edit" size={28} color="blue" />
+                      <TouchableOpacity style={styles.Editbtns} onPress={() => setIsEditing(true)}>                      
+                        <Icon name="edit" size={26} color="blue" />
+                        <Text style={{padding:5,fontSize:20,fontWeight:"normal"}}>Edit</Text>
                       </TouchableOpacity>
                     ) : (
                       <>
-                        <TouchableOpacity onPress={() => resetBasicInfo()}>
-                          <Icon name="edit" size={28} color="blue" />
+                        <TouchableOpacity style={styles.Allbtns} onPress={() => resetBasicInfo()}>
+                          <Icon name="update" size={28} color="blue" />
+                          <Text style={{padding:5,fontSize:20,fontWeight:"normal"}}>Reset</Text>
                         </TouchableOpacity>
 
-                        <TouchableOpacity onPress={() => updateBasicInfo()}>
-                          <Icon name="edit" size={28} color="blue" />
+                        <TouchableOpacity style={styles.Allbtns} onPress={() => updateBasicInfo(values)}>
+                          <Icon name="format-color-reset" size={28} color="blue" />
+                           <Text style={{padding:5,fontSize:20,fontWeight:"normal"}}>Update</Text>
                         </TouchableOpacity>
                       </>
                     )}
 
-                    {/* 
-                    <AppBtn
-                      title="Register"
-                      onPress={handleSubmit}
-                      disabled={isSubmitting}
-                    /> */}
+                   
                   </View>
 
                   <FormGroup>
+                  <Text style={styles.label}>First Name</Text>
                     <TextInput
                       style={{
                         ...styles.input,
@@ -144,14 +171,20 @@ function BasicInfo({ navigation }) {
                       <ErrorText>{errors.firstName}</ErrorText>
                     ) : null}
                   </FormGroup>
-                  {/* 
-                  <FormGroup>
+                  
+                  <FormGroup>  
+                        
+                    <Text style={styles.label}>Last Name</Text>
                     <TextInput
-                      style={styles.input}
+                      style={{
+                        ...styles.input,
+                        ...(!isEditing && styles.inputNoEditable),
+                      }}
                       value={values.lastName}
                       onChangeText={handleChange("lastName")}
                       onBlur={handleBlur("lastName")}
                       placeholder={"Last Name"}
+                      editable={isEditing ? true : false}
                     />
                     {touched.lastName && errors.lastName ? (
                       <ErrorText>{errors.lastName}</ErrorText>
@@ -159,38 +192,17 @@ function BasicInfo({ navigation }) {
                   </FormGroup>
 
                   <FormGroup>
+                  <Text style={styles.label}>Phone</Text>
                     <TextInput
-                      style={styles.input}
-                      value={values.lastName}
-                      onChangeText={handleChange("lastName")}
-                      onBlur={handleBlur("lastName")}
-                      placeholder={"Last Name"}
-                    />
-                    {touched.lastName && errors.lastName ? (
-                      <ErrorText>{errors.lastName}</ErrorText>
-                    ) : null}
-                  </FormGroup>
-
-                  <FormGroup>
-                    <TextInput
-                      style={styles.input}
-                      value={values.address}
-                      onChangeText={handleChange("address")}
-                      onBlur={handleBlur("address")}
-                      placeholder={"Address"}
-                    />
-                    {touched.address && errors.address ? (
-                      <ErrorText>{errors.address}</ErrorText>
-                    ) : null}
-                  </FormGroup>
-
-                  <FormGroup>
-                    <TextInput
-                      style={styles.input}
+                      style={{
+                        ...styles.input,
+                        ...(!isEditing && styles.inputNoEditable),
+                      }}
                       value={values.phone}
                       onChangeText={handleChange("phone")}
                       onBlur={handleBlur("phone")}
-                      placeholder={"Phone"}
+                      placeholder={"phone"}
+                      editable={isEditing ? true : false}
                     />
                     {touched.phone && errors.phone ? (
                       <ErrorText>{errors.phone}</ErrorText>
@@ -198,19 +210,79 @@ function BasicInfo({ navigation }) {
                   </FormGroup>
 
                   <FormGroup>
+                  <Text style={styles.label}>Address</Text>
                     <TextInput
-                      style={styles.input}
+                      style={{
+                        ...styles.input,
+                        ...(!isEditing && styles.inputNoEditable),
+                      }}
+                      value={values.address}
+                      onChangeText={handleChange("address")}
+                      onBlur={handleBlur("address")}
+                      placeholder={"Address"}
+                      editable={isEditing ? true : false}
+                    />
+                    {touched.address && errors.address ? (
+                      <ErrorText>{errors.address}</ErrorText>
+                    ) : null}
+                  </FormGroup>
+
+                  <FormGroup>
+                    <View>
+                  <Text style={styles.label}>Gender</Text>
+
+                    <RadioForm
+                      style={{
+                        ...styles.inputbtn,
+                        ...(!isEditing && styles.inputNoEditablebtn),
+                      }}
+                      radio_props={gender}
+                      initial={1}
+                      buttonSize={20}
+                      buttonOuterSize={30}
+                      
+                      formHorizontal={true}
+                      labelHorizontal={true}
+                      buttonColor={'#2196f3'}
+                      animation={true}
+                      onPress={(value) => {
+                        updateGender(handleChange("gender"),value);
+                     
+                    }}
+                      // value={values.phone}
+                      // onChangeText={handleChange("phone")}
+                      // onBlur={handleBlur("phone")}
+                      // placeholder={"Phone"}
+                      
+                    />
+                    
+                      
+                    
+                    {touched.phone && errors.phone ? (
+                      <ErrorText>{errors.phone}</ErrorText>
+                    ) : null}
+                    </View>
+                  </FormGroup>
+
+                  <FormGroup>
+                  <Text style={styles.label}>Bio</Text>
+                    <TextInput
+                      style={{
+                        ...styles.input,
+                        ...(!isEditing && styles.inputNoEditable),
+                      }}
                       multiline
                       minHeight={100}
                       value={values.description}
                       onChangeText={handleChange("description")}
                       onBlur={handleBlur("description")}
                       placeholder={"About yourself"}
+                      editable={isEditing ? true : false}
                     />
                     {touched.description && errors.description ? (
                       <ErrorText>{errors.description}</ErrorText>
                     ) : null}
-                  </FormGroup> */}
+                  </FormGroup>
 
                   {/* <FormGroup>
                     <Input
@@ -269,7 +341,49 @@ const styles = StyleSheet.create({
     fontSize: 18,
   },
   inputNoEditable: {
-                          backgroundColor: "#ccc",
-                          borderWidth: 0,
-                        }
+    backgroundColor: "#ccc",
+    borderWidth: 0,
+  },
+  label:{
+    fontSize:16,
+    fontWeight:'bold'
+  },
+  Allbtns:{
+    backgroundColor:"#fff",
+    borderColor:"black",
+    borderWidth:1,
+    flexDirection:"row-reverse",
+    marginRight:30,
+    justifyContent:'space-between',
+   
+  },
+  Editbtns:{
+    backgroundColor:"#fff",
+    borderColor:"black",
+    borderWidth:1,
+    flexDirection:"row-reverse",
+    marginRight:0,
+    justifyContent:'space-between',
+    
+   
+  },
+  inputNoEditablebtn:{
+    backgroundColor: "#ccc",
+    borderWidth: 0, 
+    padding: 8,
+    justifyContent:"space-between"
+    
+    
+    
+
+  },
+  inputbtn:{
+    borderColor: "#ccc",
+    borderWidth: 1,
+    padding: 8,
+    borderRadius: 4,
+    fontSize: 18,
+    justifyContent:"space-between"
+   
+  }
 });
