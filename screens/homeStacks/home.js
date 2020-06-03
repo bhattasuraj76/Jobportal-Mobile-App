e16d56e6c852fc4f034a95948232a7d9b15def95
unknown <sanjaykhatri906@gmail.com>
2020-06-04 00:09:41 +0545

    search

--- screens/homeStacks/home.js
+++ screens/homeStacks/home.js
@@ -11,6 +11,8 @@ import Loader from "../../shared/loader";
 function Home({ navigation, route }) {
   const [isLoading, setIsLoading] = useState(true);
   const [jobs, setJobs] = useState([]);
+  const [isSearching, setIsSearching] = useState(false); 
+  
 
   //async fetch jobs
   const _fetchJobs = async () => {
@@ -28,10 +30,15 @@ function Home({ navigation, route }) {
   const _searchJobs = async (data) => {
     try {
       let  url =`${apiPath}/search`
-      let data = await Axios.post(url, data).then(
+      console.log(data);
+      let response = await Axios.post(url, data).then(
+       
         (res) => res.data
+        
       );
-      if (data.resp == 1) return data.jobs;
+      console.log(response);
+      
+      if (response.resp == 1) return response.jobs.data;
     } catch (err) {
       console.error(err);
     }
@@ -46,14 +53,50 @@ function Home({ navigation, route }) {
       },
     });
   };
+  //use effect
 
-  //fetch jobs on screen foucus
+  React.useEffect(()=>{
+  console.log(route);
+  if (route.params){
+    
+    let keyword=route.params.title;
+    let category=route.params.category?[route.params.category]:null;
+    let type=route.params.type?[route.params.type]:null;
+    let level=route.params.level?[route.params.level]:null;
+    // console.log(title,category,type, level);
+    if(!keyword && !category && !type && !level)
+    return;
+
+    // console.log(title,category,type, level);
+    setIsSearching(true);
+    _searchJobs({
+      keyword, category,type,level
+    }).then(jobs=>{
+      if (jobs) setJobs(jobs);
+      console.log(jobs);
+    }).catch(err=>{
+      console.log(err);
+    });
+
+  }
+  // return()=>{
+  //   setIsSearching(false);
+
+  // }
+  
+
+
+  },[route])
+
+  // fetch jobs on screen foucus
   useFocusEffect(
     React.useCallback(() => {
+     if(isSearching)return;
       let isActive = true;
 
       _fetchJobs()
         .then(jobs => {
+          console.log('x');
           if (isActive && jobs) setJobs(jobs);
         })
         .catch(err => console.log(err))
@@ -62,28 +105,11 @@ function Home({ navigation, route }) {
       return () => {
         isActive = false;
       };
-    }, [])
+    }, [isSearching])
   );
 
-  //search jobs on getting params back from search screen
-  React.useEffect(() => {
-    if (
-      route.params?.title ||
-      route.params?.category ||
-      route.params?.type ||
-      route.params?.level
-    ) {
-      setJobs((prevJobs) => {
-        prevJobs.shift();
-        return prevJobs;
-      });
-    }
-  }, [
-    route.params?.title,
-    route.params?.category,
-    route.params?.type,
-    route.params?.level,
-  ]);
+  
+  
 
   //handle refresh and refetch jobs
   const [refreshing, setRefreshing] = React.useState(false);
@@ -94,6 +120,7 @@ function Home({ navigation, route }) {
     _fetchJobs().then((jobs) => {
       if (jobs) setJobs(jobs);
       setRefreshing(false);
+      setIsSearching(false);
     });
   }, [refreshing]);
 
